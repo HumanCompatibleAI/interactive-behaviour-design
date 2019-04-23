6a4a78cc041016593bd5751b1be10ed0b06f274e
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-04-22 17:25:53 -0700

    Look for segments in old_segs too

--- scripts/run_checks/check_prefs.py
+++ scripts/run_checks/check_prefs.py
@@ -34,13 +34,27 @@ def get_prefs():
     return json.dumps(prefs)
 
 
+def get_path(filename):
+    old_segs_dir = os.path.join(segments_dir, 'old_segs')
+    if os.path.exists(os.path.join(segments_dir, filename)):
+        return segments_dir, os.path.join(segments_dir, filename)
+    elif os.path.exists(os.path.join(old_segs_dir, filename)):
+        return old_segs_dir, os.path.join(old_segs_dir, filename)
+    else:
+        raise Exception("Could not find", filename)
+
+
 @app.route("/get_vid")
 def get_vid():
     hash = request.args['hash']
     filename = hash + '.mp4'
-    if not os.path.exists(os.path.join(segments_dir, filename)):
+    try:
+        dir, path = get_path(filename)
+    except:
         print("Warning: could not find", filename)
-    return send_from_directory(segments_dir, filename)
+        return ""
+    else:
+        return send_from_directory(dir, filename)
 
 
 @app.route("/check_pref")
@@ -48,9 +62,9 @@ def check_pref():
     h1 = request.args['h1']
     h2 = request.args['h2']
 
-    with open(os.path.join(segments_dir, h1 + '.pkl'), 'rb') as f:
+    with open(get_path(h1 + '.pkl')[1], 'rb') as f:
         r1 = pickle.load(f)
-    with open(os.path.join(segments_dir, h2 + '.pkl'), 'rb') as f:
+    with open(get_path(h2 + '.pkl')[1], 'rb') as f:
         r2 = pickle.load(f)
 
     r1s = np.array2string(np.array(r1.rewards), precision=1)
@@ -58,6 +72,7 @@ def check_pref():
     info = ("Rewards:<br />"
             "Segment 1: " + r1s + " (sum {:.1f})<br />".format(sum(r1.rewards)) +
             "Segment 2: " + r2s + " (sum {:.1f})".format(sum(r2.rewards)))
+
     if r1.obses[0].shape == (6,):
         d1 = sum(([np.linalg.norm(o) for o in r1.obses]))
         d2 = sum(([np.linalg.norm(o) for o in r2.obses]))
@@ -65,8 +80,6 @@ def check_pref():
                  "Cumulative distances from gripper to block:<br />"
                  "Segment 1: {:.3f}".format(d1) + "<br />" +
                  "Segment 2: {:.3f}".format(d2))
-    else:
-        info = ""
 
     return info
 
