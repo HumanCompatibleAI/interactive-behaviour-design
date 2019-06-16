4ed72f7e0290e926bee1f361ebca3f6ad06f790e
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-06-16 12:35:21 +0200

    Clean up throttler.py and make stat strings sort better

--- throttler.py
+++ throttler.py
@@ -5,13 +5,8 @@ from enum import Enum
 
 import easy_tf_log
 import numpy as np
-import tensorflow as tf
 
 import global_variables
-
-logger = None
-log_dir = None
-n_rl_steps_at_last_event = defaultdict(lambda: 0)
 
 
 class Stats:
@@ -20,15 +15,18 @@ class Stats:
         self.n_rl_steps_since_last_list = n_rl_steps_since_last_event_list
 
 
-event_stats = defaultdict(lambda: Stats(init_time=0, n_rl_steps_since_last_event_list=[]))
-
-
 class EventType(Enum):
     INTERACTION = 0
     REWARD_PREDICTOR_15_BATCHES = 1
 
 
-# ID necessary because we might have to initialize different throttlers for difference processes
+logger = None
+log_dir = None
+n_rl_steps_at_last_event = defaultdict(lambda: 0)
+event_stats = defaultdict(lambda: Stats(init_time=0, n_rl_steps_since_last_event_list=[]))
+
+
+# log_dir_is necessary because we might have to initialize different throttlers for difference processes
 def init(_log_dir, log_dir_id):
     global logger, log_dir
     if logger is not None:
@@ -50,11 +48,14 @@ def get_n_rl_steps_per_event(event_type):
 
 
 def mark_event(event_type):
-    print("Mark at", time.time())
     n_rl_steps = get_n_rl_steps()
     if n_rl_steps is None:
         return
+    log_stats(event_type, n_rl_steps)
+    n_rl_steps_at_last_event[event_type] = n_rl_steps
 
+
+def log_stats(event_type, n_rl_steps):
     n_rl_steps_since_last_event = n_rl_steps - n_rl_steps_at_last_event[event_type]
     # This gets called for every reward predictor training step, so we need to be careful not to log too often
     stats = event_stats[event_type]
@@ -63,26 +64,11 @@ def mark_event(event_type):
     if seconds_since_last_log > 60:
         frac = np.array(stats.n_rl_steps_since_last_list) / get_n_rl_steps_per_event(event_type)
         for aggregation_str, aggregration_fn in [('max', max), ('min', min)]:
-            logger.logkv(f'throttler/n_rl_steps_since_last_{aggregation_str}_{str(event_type).lower()}',
+            logger.logkv(f'throttler/{event_type.name}/n_rl_steps_since_last_{aggregation_str}',
                          aggregration_fn(stats.n_rl_steps_since_last_list))
-            logger.logkv(f'throttler/target_frac_{aggregation_str}_{str(event_type).lower()}',
+            logger.logkv(f'throttler/{event_type.name}/target_frac_{aggregation_str}',
                          aggregration_fn(frac))
         event_stats[event_type] = Stats(init_time=time.time(), n_rl_steps_since_last_event_list=[])
-
-    n_rl_steps_at_last_event[event_type] = n_rl_steps
-
-
-def read_events_file(events_filename):
-    events = {}
-    try:
-        for event in tf.train.summary_iterator(events_filename):
-            for value in event.summary.value:
-                if value.tag not in events:
-                    events[value.tag] = []
-                events[value.tag].append((event.wall_time, event.step, value.simple_value))
-    except Exception as e:
-        print(f"While reading '{events_filename}':", e)
-    return events
 
 
 def get_n_rl_steps():
