df3065e638ebdddf8f10cf4068e311622c3fca57
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-06-18 15:26:11 +0200

    Switch DRLHP train loop timing to LogMilliseconds

--- drlhp/training.py
+++ drlhp/training.py
@@ -12,7 +12,7 @@ import throttler
 from drlhp.pref_db import PrefDBTestTrain
 from drlhp.reward_predictor import RewardPredictor
 from global_constants import SYNC_REWARD_PREDICTOR_EVERY_N_SECONDS
-from utils import load_cpu_config, find_latest_checkpoint
+from utils import load_cpu_config, find_latest_checkpoint, LogMilliseconds
 
 
 def drlhp_load_loop(reward_predictor: RewardPredictor, ckpt_path, log_dir):
@@ -56,38 +56,29 @@ def drlhp_train_loop(make_reward_predictor_fn_cloudpickle,
             time.sleep(1.0)
             continue
 
-        t1 = time.time()
         # This /should/ be fine because we save the preference database atomically,
         # but just in case, let's be careful
         try:
-            pref_db.load(pref_db_path)
+            with LogMilliseconds('reward_predictor_train_loop/prefs_load_time_ms', logger):
+                pref_db.load(pref_db_path)
         except:
             print("Exception while loading preference database:")
             traceback.print_exc()
             time.sleep(1.0)
             continue
-        t2 = time.time()
-
         if len(pref_db) == 0:
             print("No preferences yet")
             time.sleep(1.0)
             continue
 
-        t3 = time.time()
-        try:
-            reward_predictor.train(pref_db.train, pref_db.test, val_interval=20)
-        except:
-            print("Exception while training reward predictor:")
-            traceback.print_exc()
-            time.sleep(1.0)
-            continue
-        t4 = time.time()
+        with LogMilliseconds('reward_predictor_train_loop/train_time_ms', logger):
+            try:
+                reward_predictor.train(pref_db.train, pref_db.test, val_interval=20)
+            except:
+                print("Exception while training reward predictor:")
+                traceback.print_exc()
+                time.sleep(1.0)
+                continue
 
-        load_time_ms = (t2 - t1) * 1000
-        train_time_ms = (t4 - t3) * 1000
-        ratio = train_time_ms / load_time_ms
-        logger.logkv('reward_predictor_train_loop/load_time_ms', load_time_ms)
-        logger.logkv('reward_predictor_train_loop/train_time_ms', train_time_ms)
-        logger.logkv('reward_predictor_train_loop/train_load_time_ratio', ratio)
-
-        reward_predictor.save(save_ckpt_path)
+        with LogMilliseconds('reward_predictor_train_loop/ckpt_save_time_ms', logger):
+            reward_predictor.save(save_ckpt_path)
