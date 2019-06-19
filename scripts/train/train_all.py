5ca6c0e32fb00242ffb3ae1798564a67a84a55be
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-06-19 10:45:10 +0200

    train_all: add Fetch-specific initial pref and pretrain config

--- scripts/train/train_all.py
+++ scripts/train/train_all.py
@@ -59,7 +59,8 @@ def main():
 
     seeds = list(map(int, args.seeds.split(',')))
     if args.test:
-        test_args = '--n_initial_prefs 0 --n_initial_demos 0 --pretrain_reward_predictor_seconds 0 --min_label_interval_seconds 0.01'
+        test_args = '--n_initial_prefs 0 --n_initial_demos 0 --pretrain_reward_predictor_seconds 0 ' \
+                    '--min_label_interval_seconds 0.01'
     else:
         test_args = ''
 
@@ -77,35 +78,64 @@ def main():
     else:
         run_suffix = ''
     for env_shortname, env_id in prefs_envs:
-        if 'Fetch' in env_id:
-            rollout_length_seconds = 0.5
-        elif 'Breakout' in env_id:
-            # Long enough to stretch from hitting the ball to the ball bouncing off a block
-            rollout_length_seconds = 1.5
-        else:
-            rollout_length_seconds = 1.0
+        rollout_len_config = get_rollout_len_config(env_id)
+        pretrain_config = get_pretrain_config(env_id)
+        initial_prefs_config = get_initial_prefs_config(env_id)
         experiments = get_experiments(env_id)
         for ex in experiments:
+            redo_config = f"{'--disable_redo' if ex.disable_redo else ''} "
+            decay_config = f"{'--decay_label_rate' if ex.decay_label_rate else ''} "
+            extra_args = f" {rollout_len_config} {ex.no_primitives_config} {args.extra_args}"
             wandb_group = f"{env_shortname}-{ex.name}{run_suffix}_{git_rev}"
-            extra_args = f" --rollout_length_seconds {rollout_length_seconds} {ex.no_primitives_config} {args.extra_args}"
             for seed in seeds:
                 run_name = f"{env_shortname}-{seed}-{ex.name}{run_suffix}"
                 if args.test:
                     run_name += '_test'
                 cmd = ("python3 scripts/train/auto_train_prefs.py "
-                       f"{env_id} {ex.train_mode} {ex.segment_generation} {run_name} {test_args} "
+                       f"{env_id} {ex.train_mode} {ex.segment_generation} {run_name} "
+                       f"{test_args} {redo_config} {decay_config} {pretrain_config} {initial_prefs_config} "
+                       f"{args.harness_extra_args}"
                        f"--seed {seed} "
-                       f"{'--disable_redo' if ex.disable_redo else ''} "
-                       f"{'--decay_label_rate' if ex.decay_label_rate else ''} "
-                       f"--extra_args ' {extra_args}' --gpus '{args.gpus}' {args.harness_extra_args} --tags {env_shortname},{ex.name} --group {wandb_group}")
+                       f"--extra_args ' {extra_args}' --gpus '{args.gpus}' "
+                       f"--tags {env_shortname},{ex.name} --group {wandb_group}")
                 print(cmd)
+
+
+def get_initial_prefs_config(env_id):
+    if 'Fetch' in env_id:
+        n = 100
+    else:
+        n = 500
+    return f'--n_initial_prefs {n}'
+
+
+def get_pretrain_config(env_id):
+    if 'Fetch' in env_id:
+        n_epochs = 10
+    else:
+        n_epochs = 200
+    return f'--pretrain_reward_predictor_epochs {n_epochs}'
+
+
+def get_rollout_len_config(env_id):
+    if 'Fetch' in env_id:
+        rollout_length_seconds = 0.5
+    elif 'Breakout' in env_id:
+        # Long enough to stretch from hitting the ball to the ball bouncing off a block
+        rollout_length_seconds = 1.5
+    else:
+        rollout_length_seconds = 1.0
+    rollout_len_config = f"--rollout_length_seconds {rollout_length_seconds}"
+    return rollout_len_config
 
 
 def get_experiments(env_id):
     if 'Breakout' in env_id:
-        no_primitives_config = '--cur_policy_randomness correlated_random_action --rollout_random_action_prob 0.8 --rollout_random_correlation 0.7'
+        no_primitives_config = '--cur_policy_randomness correlated_random_action ' \
+                               '--rollout_random_action_prob 0.8 --rollout_random_correlation 0.7'
     elif 'Enduro' in env_id:
-        no_primitives_config = '--cur_policy_randomness correlated_random_action --rollout_random_action_prob 1.0 --rollout_random_correlation 0.99'
+        no_primitives_config = '--cur_policy_randomness correlated_random_action ' \
+                               '--rollout_random_action_prob 1.0 --rollout_random_correlation 0.99'
     else:
         no_primitives_config = ''
 
@@ -114,7 +144,7 @@ def get_experiments(env_id):
     else:
         imitation_learning_disable_redo = False
 
-    experiments = []
+    experiments = list()
 
     experiments.append(Experiment(
         name='drlhp',
