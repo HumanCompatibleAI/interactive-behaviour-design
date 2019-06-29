fb5ae8ecdcaf8bb7abce5e49d222bf8d9167d831
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-06-29 14:41:45 +0200

    plot_true_and_predicted_rewards: plot Boltzmann-normalized rewards

--- scripts/run_checks/plot_true_and_predicted_actions.py
+++ scripts/run_checks/plot_true_and_predicted_actions.py
@@ -2,6 +2,7 @@ import argparse
 from time import sleep
 
 from pylab import *
+from scipy.special import logsumexp
 
 parser = argparse.ArgumentParser()
 parser.add_argument('log')
@@ -25,13 +26,24 @@ for episode_n in range(args.start, 100000000000):
     data = np.array([list(map(float, l.split(' '))) for l in elines])
     plot(data[:, 0], label='Environment reward')
 
+    data[:, 1] += np.random.randint(-5, 5)
+    data[:, 1] *= np.random.rand() * 2
+
     plot(data[:, 1], label='Predicted reward')
 
     env_rew_min, env_rew_max = min(data[:, 0]), max(data[:, 0])
     delta = env_rew_max - env_rew_min
-    pred = data[:, 1]
-    pred = (pred - min(pred)) / (max(pred) - min(pred)) * (env_rew_max - env_rew_min) + env_rew_min
-    plot(pred, label='Predicted reward (rescaled)')
+    predicted_rewards = data[:, 1]
+    predicted_rewards = (predicted_rewards - min(predicted_rewards)) / (max(predicted_rewards) - min(predicted_rewards)) * (env_rew_max - env_rew_min) + env_rew_min
+    plot(predicted_rewards, label='Predicted reward (rescaled)')
+
+    predicted_rewards = data[:, 1]
+    predicted_rewards = log(exp(predicted_rewards - logsumexp(predicted_rewards)))
+    plot(predicted_rewards, label='Predicted reward (Boltzmann)')
+
+    ylim([-10, 4])
+
+
     legend()
     s = input()
     if s == 'q':
