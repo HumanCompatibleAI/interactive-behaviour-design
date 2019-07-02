One more attempt at fixing extreme state normalization scale

drlhp/reward_predictor.py
@@ -251,9 +251,9 @@ class RewardPredictor:
 
         mean_std = global_variables.predicted_rewards_normalize_mean_std
         if mean_std is not None:
-            shift, scale = map(float, mean_std.split(','))
-            scale *= scale
-            shift += shift
+            mean, std = map(float, mean_std.split(','))
+            scale *= std
+            shift += mean
 
         if self.reward_call_n % global_variables.log_reward_normalization_every_n_calls == 0:
             self.logger.logkv('reward_predictor/reward_cur_batch_min', np.min(rewards))