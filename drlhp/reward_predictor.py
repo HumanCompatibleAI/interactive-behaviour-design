nth attempt at fixing extreme state predicted reward normalization

drlhp/reward_predictor.py
@@ -244,16 +244,23 @@ class RewardPredictor:
             min_reward = np.min(rewards)
 
         delta = max_reward - min_reward
-        if delta == 0:
-            return rewards
-        scale = 1 / delta
-        shift = - (min_reward + max_reward) / 2
-
-        mean_std = global_variables.predicted_rewards_normalize_mean_std
-        if mean_std is not None:
-            mean, std = map(float, mean_std.split(','))
-            scale *= std
-            shift += mean
+        specified_min_max = global_variables.predicted_rewards_normalize_min_max
+        if specified_min_max is not None:
+            specified_min, specified_max = map(float, specified_min_max.split(','))
+            scale = (specified_max - specified_min) / delta
+            shift = specified_min - min_reward
+            rewards -= min_reward
+            rewards *= scale
+            rewards += specified_min
+        else:
+            # Reward should span a range of 2, centered at 0
+            if delta == 0:
+                return rewards
+            scale = 2 / delta
+            shift = (min_reward + max_reward) / 2
+            rewards -= min_reward
+            rewards *= scale
+            rewards += shift
 
         if self.reward_call_n % global_variables.log_reward_normalization_every_n_calls == 0:
             self.logger.logkv('reward_predictor/reward_cur_batch_min', np.min(rewards))
@@ -263,8 +270,6 @@ class RewardPredictor:
             self.logger.logkv('reward_predictor/scale', scale)
             self.logger.logkv('reward_predictor/shift', shift)
 
-        rewards *= scale
-        rewards += shift
 
         return rewards
 

drlhp/reward_predictor_test.py
@@ -3,18 +3,21 @@ import os
 import sys
 import tempfile
 import unittest
+from typing import List
 
 import numpy as np
 import tensorflow as tf
 from gym.spaces import Box
+from matplotlib.pyplot import plot, legend, show, grid
 
-sys.path.insert(0, '..')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from drlhp.reward_predictor_core_network import net_mlp, net_cnn
 from drlhp.pref_db import PrefDB
 from drlhp.reward_predictor import RewardPredictor, MIN_L2_REG_COEF, PredictedRewardNormalization
 import global_variables
 import throttler
+from utils import ObsRewardTuple, load_reference_trajectory
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -91,6 +94,41 @@ class SmokeTests(unittest.TestCase):
 
             for _ in range(100):
                 rp.train(prefs_train, prefs_val, val_interval=1, verbose=verbose)
+
+
+class TestRewardNormalization(unittest.TestCase):
+    def _test(self):
+        ckpt_path = '/private/var/folders/z2/5bbsrgpj7y51rhv12n7w9q9h0000gn/T/tmp.LDcnFZqR/reward_predictor.ckpt.84'
+        obs_space = Box(low=float('-inf'), high=float('inf'), shape=(6,), dtype=np.float32)
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            global_variables.log_reward_normalization_every_n_calls = 1000
+            global_variables.predicted_rewards_normalize_min_max = '-3.83,-0.05'
+            rp = RewardPredictor(obs_space=obs_space,
+                                 network=net_mlp,
+                                 network_args={},
+                                 r_std=1.0,
+                                 log_dir=tmp_dir,
+                                 seed=0,
+                                 reward_normalization=PredictedRewardNormalization.EXTREME_TRAINING_STATES,
+                                 normalization_loss_coef=0,
+                                 name='test')
+            rp.load(ckpt_path)
+
+            env_id = 'FetchReach-CustomActionRepeat5ActionLimit0.2-v0'
+            reference_trajectory = load_reference_trajectory(env_id)  # type: List[ObsRewardTuple]
+            obses = np.array([tup.obs for tup in reference_trajectory])
+            env_rewards = [tup.reward for tup in reference_trajectory]
+            print("Environment env_rewards range, scale:", np.min(env_rewards), np.max(env_rewards))
+
+            pred_rewards = rp.normalized_rewards(obses)
+            print("Predicted pred_rewards mean, std:", np.min(pred_rewards), np.max(env_rewards))
+
+            plot(env_rewards, label='Environment rewards')
+            plot(pred_rewards, label='Predicted rewards')
+            grid()
+            legend()
+            show()
+
 
 
 class TestRewardPredictor(unittest.TestCase):

run.py
@@ -5,7 +5,6 @@ import glob
 import multiprocessing
 import os
 import os.path as osp
-import pickle
 import platform
 import random
 import re
@@ -37,7 +36,7 @@ from checkpointer import Checkpointer
 from classifier_buffer import ClassifierDataBuffer
 from classifier_collection import ClassifierCollection
 from drlhp.pref_db import PrefDBTestTrain
-from drlhp.reward_predictor import RewardPredictor, PredictedRewardNormalization
+from drlhp.reward_predictor import RewardPredictor
 from drlhp.reward_predictor_core_network import net_mlp, net_cnn
 from drlhp.training import drlhp_train_loop, drlhp_load_loop
 from env import make_envs
@@ -51,7 +50,7 @@ from reward_switcher import RewardSelector
 from rollouts import RolloutsByHash, CompressedRollout
 from subproc_vec_env_custom import SubprocVecEnvNoAutoReset
 from utils import find_latest_checkpoint, MemoryProfiler, configure_cpus, \
-    load_cpu_config, register_debug_handler, get_available_gpu_ns, ObsRewardTuple
+    load_cpu_config, register_debug_handler, get_available_gpu_ns, ObsRewardTuple, load_reference_trajectory
 from web_app.app import run_web_app
 from web_app.comparisons import monitor_segments_dir_loop, write_segments_loop
 from wrappers import seaquest_reward, fetch_pick_and_place_register, lunar_lander_reward, breakout_reward, enduro, \
@@ -90,15 +89,6 @@ def check_env(env_id):
         raise Exception(f"Env {env_id} not supported; try", ','.join(supported_envs))
 
 
-def load_reference_trajectory(env_id):
-    if env_id == 'FetchReach-CustomActionRepeat5ActionLimit0.2-v0':
-        with open('reference_trajectories/reach_reference_trajectory.pkl', 'rb') as f:
-            traj = pickle.load(f)
-    else:
-        traj = None
-    return traj
-
-
 def calculate_slopes(num_list):
     num_list = np.array(num_list)
     return num_list[1:] - num_list[:-1]
@@ -113,10 +103,6 @@ def predict_reference_trajectory_reward_loop(reference_trajectory: List[ObsRewar
     log_file = open(os.path.join(reference_trajectory_rewards_log_dir, 'predicted_rewards.txt'), 'w')
     imgs_dir = os.path.join(log_dir, 'reference_trajectory_reward_images')
     os.makedirs(imgs_dir)
-
-    if np.array(obses).shape[1] == 9:
-        # Recorded using old env
-        obses = np.array(obses)[:, :6]
 
     test_n = 0
 