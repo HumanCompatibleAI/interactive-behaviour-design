2e083f1df0fd8f3a46b375952af418060a4b584f
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-05 15:20:58 +0200

    Fix Docker push command

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Before running, do
-#   aws ecr get-login --region us-west-2 --no-include-email
-# and run the resulting Docker command to authenticate with the AWS registry
-
 set -e
 
 if ! git status | grep -q 'working tree clean'; then
@@ -12,5 +8,4 @@ if ! git status | grep -q 'working tree clean'; then
 fi
 
 docker build -t ibd .
-docker tag ibd:latest 109526153624.dkr.ecr.us-west-2.amazonaws.com/repository-0:latest
-docker push 109526153624.dkr.ecr.us-west-2.amazonaws.com/repository-0:latest
+docker push mrahtz/ibd
